94cda56914aec2bf627cb1fccb1060921f338244
gamedekdee <gamedekdee05@gmail.com>
2025-07-11 21:14:41 +0700

    Remove unused auth import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 // pages/index.js
 import { useEffect, useState } from "react"
-import { db, auth } from "../lib/firebase"
 import { collection, query, where, getDocs, doc, updateDoc } from "firebase/firestore"
 import { useRouter } from "next/router"
 
