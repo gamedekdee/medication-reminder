b8ef369999142effa56802e84abd06076ec7ce6f
gamedekdee <gamedekdee05@gmail.com>
2025-07-11 21:33:34 +0700

    Fix imports for global CSS and usePushSubscription hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-import "../styles/globals.css"
-import usePushSubscription from "../hooks/usePushSubscription"
+import "../styles/globals.css"           // โหลด CSS ที่คุณสร้าง
+import usePushSubscription from "../hooks/usePushSubscription"  // โหลด hook ใหม่
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "../lib/firebase"
 import { useState, useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState(null)
-  useEffect(() => onAuthStateChanged(auth, u => setUser(u)), [])
+  const { user } = pageProps // สมมติคุณผ่าน user มา
   usePushSubscription(user)
-
-  return <Component {...pageProps} user={user} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp
